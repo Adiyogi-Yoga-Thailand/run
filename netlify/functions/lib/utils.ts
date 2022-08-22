538c7e83a0bb336c0fa56fcda8a6e21f6bc84a88
Songpol Anannetikul <songpol.anannetikul@gmail.com>
2022-08-22 22:11:34 +0700

    Fix 500 error on success

--- netlify/functions/lib/utils.ts
+++ netlify/functions/lib/utils.ts
@@ -1,5 +1,4 @@
 import { HandlerEvent, HandlerResponse } from "@netlify/functions"
-// import { GoogleApis } from "googleapis"
 
 export function checkHttpMethod(event: HandlerEvent, httpMethod: string): [boolean, any] {
   if (event.httpMethod === httpMethod) {
@@ -27,17 +26,3 @@ export async function handleRequest(
     return { statusCode: 500, body: JSON.stringify(e) }
   }
 }
-
-// export async function auth(google: GoogleApis) {
-//   const auth = await new google.auth.GoogleAuth({
-//     // Scopes can be specified either as an array or as a single, space-delimited string.
-//     scopes: [
-//       "https://www.googleapis.com/auth/drive",
-//       "https://www.googleapis.com/auth/drive.file",
-//       "https://www.googleapis.com/auth/drive.readonly",
-//       "https://www.googleapis.com/auth/spreadsheets",
-//       "https://www.googleapis.com/auth/spreadsheets.readonly",
-//     ],
-//   }).getClient()
-//   google.options({ auth })
-// }
